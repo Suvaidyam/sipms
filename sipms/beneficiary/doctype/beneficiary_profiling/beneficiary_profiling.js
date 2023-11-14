70eba0588e623857fa78bfe295373d497a2a7535
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-11-14 17:45:34 +0530

    add

--- sipms/beneficiary/doctype/beneficiary_profiling/beneficiary_profiling.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, suvaidyam and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Beneficiary Profiling", {
-// 	refresh(frm) {
-
-// 	},
-// });
