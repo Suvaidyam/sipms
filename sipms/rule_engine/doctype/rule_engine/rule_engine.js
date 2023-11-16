9ab07d7a406e56bd46e61eab37b6d47bcb399a6a
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-11-16 16:49:12 +0530

    Add

--- sipms/rule_engine/doctype/rule_engine/rule_engine.js
+++ sipms/rule_engine/doctype/rule_engine/rule_engine.js
@@ -1,23 +1,45 @@
 // Copyright (c) 2023, suvaidyam and contributors
 // For license information, please see license.txt
 
+function get_field_list(frm){
+    frappe.call({
+        method: "sipms.rule_engine.apis.get_meta_api.get_field_lists",
+        args: {
+            doctype_name: "Beneficiary Profiling",
+            field_types: ["Date","Int"]
+        },
+        callback: function(response) {
+            // Handle the response
+            frm.fields_dict.field_table.grid.update_docfield_property("rule_field","options", response.message);
+            if (response.message) {
+                console.log(response.message);
+            } else {
+                console.error("API call failed");
+            }
+        }});
+}
+
 frappe.ui.form.on("Rule Engine", {
 	refresh(frm) {
-        frappe.call({
-            method: "sipms.rule_engine.apis.get_meta_api.get_field_lists",
-            args: {
-                doctype_name: "Beneficiary Profiling",
-                field_types: ["Date","Int" ]
-            },
-            callback: function(response) {
-                // Handle the response
-                if (response.message) {
-                    console.log(response.message);
-                    frm.fields_dict.field_table.grid.update_docfield_property("field","options", response.message);
-                    // Do something with the response data
-                } else {
-                    console.error("API call failed");
-                }
-            }});
+
 	},
 });
+// CHILD TABLE 
+frappe.ui.form.on('Rule Engine Child', {
+    refresh(frm) {
+    },
+    field_table_add(frm, cdt, cdn) {
+    let initial_code = 64
+    let row = frappe.get_doc(cdt, cdn);
+    if(row.idx <= 26){
+        row.code= (String.fromCharCode(initial_code + row.idx))
+    }else{
+        row.code= (String.fromCharCode(initial_code + (row.idx -26)) + String.fromCharCode(initial_code + (row.idx- 26)))
+    }
+    
+    get_field_list(frm)
+    console.log(row)
+      // set_field_options("specific_support_type", ["Loan Approved","Loan Appealing"])
+  
+    }
+  })
