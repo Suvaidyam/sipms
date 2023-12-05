49c8ba4146a38378c536396034bcac0e2f3af9ab
amreshyadavgit <ami92.raj@gmail.com>
2023-12-05 18:26:18 +0000

    rule engine expression logic handled

--- sipms/rule_engine/doctype/rule_engine/rule_engine.js
+++ sipms/rule_engine/doctype/rule_engine/rule_engine.js
@@ -1,26 +1,56 @@
 // Copyright (c) 2023, suvaidyam and contributors
 // For license information, please see license.txt
+
+function evaluateExpression(input, expression) {
+    if (!(/^[a-zA-Z0-9\s()+\-/*%&|=!<>]*$/.test(expression))) {
+        return 'Invalid expression'
+    }
+    // expression = expression.toLowerCase();
+    expression = expression.replace(/and/g, '&&').replace(/or/g, '||');
+    expression = expression.replace(/AND/g, '&&').replace(/OR/g, '||');
+    for (let key in input) {
+        expression = expression.replace(new RegExp(key, 'g'), input[key]);
+    }
+    try {
+        return eval(expression);
+    } catch (err) {
+        return err.message;
+    }
+}
+function generateQueryString(rows, expression) {
+
+    for (let row of rows) {
+        if (['IN', 'NOT IN'].includes(row.operator)) {
+            let val = row.data?.split(',').map(e => `'${e}'`).join(',');
+            expression = expression.replace(new RegExp(row.code, 'g'), `${row.rule_field} ${row.operator} (${val})`)
+        } else {
+            expression = expression.replace(new RegExp(row.code, 'g'), `${row.rule_field} ${row.operator} '${row.data}'`)
+        }
+    }
+    return 'select * from `tabBeneficiary Profiling` where ' + expression
+}
 var field_list = []
-function get_field_list(frm){
+function get_field_list(frm) {
     frappe.call({
         method: "sipms.rule_engine.apis.get_meta_api.get_field_lists",
         args: {
             doctype_name: "Beneficiary Profiling",
-            field_types: ["Date","Int", "Link"]
+            field_types: ["Date", "Int", "Link"]
         },
-        callback: function(response) {
+        callback: function (response) {
             // Handle the response
             field_list = response.message
-            frm.fields_dict.field_table.grid.update_docfield_property("rule_field","options", response.message);
+            frm.fields_dict.field_table.grid.update_docfield_property("rule_field", "options", response.message);
             if (response.message) {
                 console.log(response.message);
             } else {
                 console.error("API call failed");
             }
-        }});
-        
+        }
+    });
+
 }
-function get_Link_list(doctype_name , frm){
+function get_Link_list(doctype_name, frm) {
     frappe.call({
         method: "frappe.desk.search.search_link",
         args: {
@@ -28,81 +58,104 @@ function get_Link_list(doctype_name , frm){
             txt: "",
             page_length: 10000
         },
-        callback: function(response) {
+        callback: function (response) {
             // Handle the response
-            frm.fields_dict.field_table.grid.update_docfield_property("select","options", response.results);
+            frm.fields_dict.field_table.grid.update_docfield_property("select", "options", response.results);
             if (response.results) {
                 console.log(response.results);
             } else {
                 console.error("API call failed");
             }
-        }});
-        
+        }
+    });
+
 }
 frappe.ui.form.on("Rule Engine", {
-	refresh(frm) {
+    refresh(frm) {
 
-	},
+    },
+    condition: (frm) => {
+        let el = document.querySelector("[data-fieldname='condition'] p")
+        let rows = frm.get_field('field_table')?.grid?.data;
+        if (rows && rows?.length) {
+            let input = {};
+            for (let row of rows) {
+                input[row.code] = true
+            }
+            let eval = evaluateExpression(input, frm.doc.condition);
+            if (typeof eval == 'string') {
+                // el.style.color = 'red !important';
+                el.setAttribute("style", "color: red !important");
+                el.textContent = eval;
+            } else {
+                el.setAttribute("style", "color: green !important");
+                el.textContent = generateQueryString(rows, frm.doc.condition);
+            }
+        } else if (frm.doc.condition) {
+            el.setAttribute("style", "color: red !important");
+            el.textContent = 'Invalid expression.';
+        } else {
+            el.setAttribute("style", "");
+            el.textContent = '';
+        }
+    }
 });
-// CHILD TABLE 
+// CHILD TABLE
 frappe.ui.form.on('Rule Engine Child', {
     refresh(frm) {
     },
     field_table_add(frm, cdt, cdn) {
-    let initial_code = 64
-    let row = frappe.get_doc(cdt, cdn);
-    if(row.idx <= 26){
-        row.code= (String.fromCharCode(initial_code + row.idx))
-    }else{
-        row.code= (String.fromCharCode(initial_code + (row.idx -26)) + String.fromCharCode(initial_code + (row.idx- 26)))
-    }
-    
-    get_field_list(frm)
-    console.log("aa",row)
-    // frm.fields_dict.field_table.grid.toggle_reqd("data", 1)
-    // console.log(frm.fields_dict.field_table.grid.get_field('rule_field'))
-    // frm.fields_dict.field_table.section.fields_dict.field_table.grid.df.fields_dict.rule_field.toggle(false)
-    // frm.fields_dict.field_table.grid.df.fields_dict.rule_field.toggle(false);
-    // frm.fields_dict.field_table.grid.get_field('rule_field').df.hidden = true;
+        let initial_code = 64
+        let row = frappe.get_doc(cdt, cdn);
+        if (row.idx <= 26) {
+            row.code = (String.fromCharCode(initial_code + row.idx))
+        } else {
+            row.code = (String.fromCharCode(initial_code + (row.idx - 26)) + String.fromCharCode(initial_code + (row.idx - 26)))
+        }
+
+        get_field_list(frm)
+        console.log("aa", row)
+        // frm.fields_dict.field_table.grid.toggle_reqd("data", 1)
+        // console.log(frm.fields_dict.field_table.grid.get_field('rule_field'))
+        // frm.fields_dict.field_table.section.fields_dict.field_table.grid.df.fields_dict.rule_field.toggle(false)
+        // frm.fields_dict.field_table.grid.df.fields_dict.rule_field.toggle(false);
+        // frm.fields_dict.field_table.grid.get_field('rule_field').df.hidden = true;
 
 
-    // frm.fields_dict.field_table.grid.df.read_only = 1
-    // frm.fields_dict.field_table.row.data.df.data = 'Date';
-    // console.log(frm.fields_dict.field_table.section.fields_dict.field_table)
-   
-  
+        // frm.fields_dict.field_table.grid.df.read_only = 1
+        // frm.fields_dict.field_table.row.data.df.data = 'Date';
+        // console.log(frm.fields_dict.field_table.section.fields_dict.field_table)
+
+
     },
     rule_field: function (frm, cdt, cdn) {
         let row = frappe.get_doc(cdt, cdn);
-        row.type= field_list?.find(f => f.value == row.rule_field)?.type;
-        if(row.type == "Link"){
-            frm.fields_dict.field_table.grid.update_docfield_property("operator","options", ["=", "!=", "IN","NOT IN"]);
-            get_Link_list(field_list.find(f => f.value == row.rule_field)?.options , frm)
-        }else{
-            frm.fields_dict.field_table.grid.update_docfield_property("operator","options", ["=", "!=",">","<",">=","<=", "IN","NOT IN"]);
+        row.type = field_list?.find(f => f.value == row.rule_field)?.type;
+        if (row.type == "Link") {
+            frm.fields_dict.field_table.grid.update_docfield_property("operator", "options", ["=", "!=", "IN", "NOT IN"]);
+            get_Link_list(field_list.find(f => f.value == row.rule_field)?.options, frm)
+        } else {
+            frm.fields_dict.field_table.grid.update_docfield_property("operator", "options", ["=", "!=", ">", "<", ">=", "<=", "IN", "NOT IN"]);
         }
         frm.fields_dict.field_table.grid.refresh();
-        console.log( );
-        console.log(row.rule_field)
-
-        var cur_grid =frm.get_field('field_table').grid;
-var cur_doc = locals[cdt][cdn];
-var cur_row = cur_grid.get_row(cur_doc.name);
-cur_row.toggle_view();
+        var cur_grid = frm.get_field('field_table').grid;
+        var cur_doc = locals[cdt][cdn];
+        var cur_row = cur_grid.get_row(cur_doc.name);
+        cur_row.toggle_view();
     },
-    date:function(frm , cdt , cdn){
+    date: function (frm, cdt, cdn) {
         let row = frappe.get_doc(cdt, cdn);
         row.data = row.date
         frm.fields_dict.field_table.grid.refresh();
     },
-    select:function(frm , cdt, cdn){
+    select: function (frm, cdt, cdn) {
         let row = frappe.get_doc(cdt, cdn);
         row.data = row.select
         frm.fields_dict.field_table.grid.refresh();
     },
-    value:function(frm , cdt , cdn){
+    value: function (frm, cdt, cdn) {
         let row = frappe.get_doc(cdt, cdn);
         row.data = row.value
         frm.fields_dict.field_table.grid.refresh();
     }
-  })
+})
