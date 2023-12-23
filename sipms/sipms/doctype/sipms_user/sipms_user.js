db4998adc29cf07fd8a909fa026f577be3aaf7bb
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-12-23 11:10:21 +0530

    add client side script

--- sipms/sipms/doctype/sipms_user/sipms_user.js
+++ sipms/sipms/doctype/sipms_user/sipms_user.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2023, suvaidyam and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Sipms User", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Sipms User", {
+	refresh(frm) {
+        
+	},
+    role_profile:function(frm){
+        
+    }
+});
