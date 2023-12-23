e849ee0c929b2e6c4da1b021f5b6a7e555cbc7d7
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-12-23 10:49:40 +0530

    sipms user and add update sipms user done

--- sipms/sipms/doctype/sipms_user/sipms_user.py
+++ sipms/sipms/doctype/sipms_user/sipms_user.py
@@ -4,15 +4,6 @@
 import frappe
 from frappe.model.document import Document
 
-def update_new_user_password(email , password):
-	# frappe.db.set_value('User', email, 'new_password', password, update_modified=False)
-	user_doc = frappe.get_doc("User", email)
-	print("user ???????///////////////////////////////", user_doc)
-	user_doc.new_password = password
-	user_doc.save()
-	return user_doc
-
-
 class SipmsUser(Document):
 	def after_insert(self):
 		new_user = frappe.new_doc("User")
@@ -20,9 +11,17 @@ class SipmsUser(Document):
 		new_user.first_name = self.first_name
 		new_user.last_name = self.last_name
 		new_user.role_profile_name = self.role_profile
-		new_user.new_password = self.password
+		new_user.user_image = self.user_image
+		new_user.new_password = self.confirm_password
 		new_user.save()
-		# update password of users
-		# update_new_user_password(self.email , self.password)
 
 
+	def on_update(self):
+		user_doc = frappe.get_doc("User", self.email)
+		user_doc.email = self.email
+		user_doc.first_name = self.first_name
+		user_doc.last_name = self.last_name
+		user_doc.role_profile_name = self.role_profile
+		user_doc.user_image = self.user_image
+		user_doc.new_password = self.confirm_password
+		user_doc.save()
