36539830357fe275d354dc6e71773a87b6a8545c
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-12-24 09:04:35 +0530

    add other logic in current house types

--- sipms/sipms/doctype/beneficiary_profiling/beneficiary_profiling.py
+++ sipms/sipms/doctype/beneficiary_profiling/beneficiary_profiling.py
@@ -8,14 +8,15 @@ from sipms.services.family import family
 
 class BeneficiaryProfiling(Document):
 	def after_insert(self):
-		# source of informtion other logics
 		if(self.new_source_of_information):
-			new_source_of_information_doc = frappe.new_doc("Current Occupation")
+			new_source_of_information_doc = frappe.new_doc("Source Of Information")
 			new_source_of_information_doc.source_name = self.new_source_of_information
 			new_source_of_information_doc.save()
-
-		# other logics for other house types
-		print(self.has_anyone_from_your_family_visisted_before)
+		if(self.current_house_type):
+			current_house_type_doc = frappe.new_doc("House Types")
+			current_house_type_doc.house_type_name = self.current_house_type
+			current_house_type_doc.save()
+		# print(self.has_anyone_from_your_family_visisted_before)
 		if(self.has_anyone_from_your_family_visisted_before == "No"):
 			family_doc = family.create(self)
 			frappe.db.set_value('Beneficiary Profiling', self.name, 'select_primary_member', family_doc.name, update_modified=False)
