d5903983fb86f3351d42605a3cfaec61727f2585
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-12-24 21:16:59 +0530

    add

--- sipms/sipms/doctype/beneficiary_profiling/beneficiary_profiling.py
+++ sipms/sipms/doctype/beneficiary_profiling/beneficiary_profiling.py
@@ -24,9 +24,8 @@ class BeneficiaryProfiling(Document):
 			current_house_type_doc.save()
 		# print(self.has_anyone_from_your_family_visisted_before)
 		if(self.has_anyone_from_your_family_visisted_before == "No"):
-			""
-			# family_doc = family.create(self)
-			# frappe.db.set_value('Beneficiary Profiling', self.name, 'select_primary_member', family_doc.name, update_modified=False)
+			family_doc = family.create(self)
+			frappe.db.set_value('Beneficiary Profiling', self.name, 'select_primary_member', family_doc.name, update_modified=False)
 	
 
 	def on_update(self):
