4f03e57cd9446ff9d3ca0f9b71caac1ba8cb86bf
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-12-24 16:55:00 +0530

    add user manage

--- sipms/utils/login_user_details.py
+++ sipms/utils/login_user_details.py
@@ -3,8 +3,10 @@ import frappe
 class  LoginUser:
     def get_single_windows():
         usr = frappe.get_doc("Sipms User", frappe.session.user)
-        return usr.single_window
+        single_window = usr.single_window
+        return single_window
 
     def get_helpdesk():
         usr = frappe.get_doc("Sipms User", frappe.session.user)
-        return usr.help_desk
+        help_desk = usr.help_desk
+        return help_desk
