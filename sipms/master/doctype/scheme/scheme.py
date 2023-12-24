4b8300d1cb884fc03034d7f1f2b466af8cd40a29
amreshyadavgit <ami92.raj@gmail.com>
2023-12-24 12:25:17 +0000

    changes

--- sipms/master/doctype/scheme/scheme.py
+++ sipms/master/doctype/scheme/scheme.py
@@ -3,7 +3,50 @@
 
 # import frappe
 from frappe.model.document import Document
-
-
+import re
+from frappe import _
+import frappe
 class Scheme(Document):
-	pass
+	def validate(self):
+		if self.condition:
+			try:
+				if evaluate_expression([],self.condition):
+					pass
+				else:
+					frappe.msgprint(_("Not a valid condition"), raise_exception=1)
+			except Exception as e:
+				frappe.msgprint(_(e), raise_exception=1)
+		else:
+			pass
+
+	def evaluate_expression(input_dict, expression):
+		if not re.match(r"^[a-zA-Z0-9\s()+\-/*%&|=!<>]*$", expression):
+			return 'Invalid expression'
+
+		# expression = expression.lower()
+		expression = expression.replace('and', 'and').replace('or', 'or')
+		expression = expression.replace('AND', 'and').replace('OR', 'or')
+
+		for key, value in input_dict.items():
+			expression = expression.replace(key, str(value))
+
+		try:
+			return eval(expression)
+		except Exception as err:
+			return str(err)
+
+	def generate_query_string(rows, __expression):
+		expression = __expression.replace('and', '#').replace('or', '%')
+		expression = expression.replace('AND', '#').replace('OR', '%')
+
+		for row in rows:
+			if row['operator'] in ['IN', 'NOT IN']:
+				val = ','.join([f"'{e.strip()}'" for e in row['data'].split(',')]) if row['data'] else ''
+				expression = expression.replace(row['code'], f"{row['rule_field']} {row['operator']} ({val})")
+			else:
+				expression = expression.replace(row['code'], f"{row['rule_field']} {row['operator']} '{row['data']}'")
+
+		expression = expression.replace('#', 'and').replace('%', 'or')
+		return f"select * from `tabBeneficiary Profiling` where {expression}"
+
+
