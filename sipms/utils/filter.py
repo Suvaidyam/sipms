3ae4f4afa829030cb86a0be0b57164a6af4c39bc
amreshyadavgit <ami92.raj@gmail.com>
2024-01-03 10:28:03 +0000

    changes

--- sipms/utils/filter.py
+++ sipms/utils/filter.py
@@ -3,6 +3,22 @@ from sipms.utils.cache import Cache
 
 
 class Filter:
+    def set_query_filters(doctype=None):
+        roles = frappe.get_roles(frappe.session.user)
+        if doctype is not None and ("Administrator" not in roles):
+            cond_str = ""
+            if "Admin" in roles:
+                cond_str = "state"
+            elif "CSC Member" in roles:
+                cond_str = "single_window"
+            elif "Help-desk member" in roles:
+                cond_str = "help_desk"
+            elif "MIS executive" in roles:
+                cond_str = "single_window"
+            value = Cache.get_csc()
+            return f"(`tab{doctype}`.{cond_str} = '{value}')"
+        return ""
+
     def set_report_filters(filters=None, date_column='creation', str=False, table_name='', csc_filter=True):
         new_filters = {}
         str_list = []
