911674e0aa33fc529472123d250d2106f286f85e
Abhishek kumar <abhishek.suvaidyam@gmail.com>
2024-01-16 07:41:40 +0000

    add changes

--- sipms/sipms/report/milestone_wise_demands/milestone_wise_demands.py
+++ sipms/sipms/report/milestone_wise_demands/milestone_wise_demands.py
@@ -50,13 +50,6 @@ def execute(filters=None):
             "fieldtype": "Data",
             "width": 130,
         },
-
-        {
-            "fieldname": "count",
-            "label": "Total Count",
-            "fieldtype": "Int",
-            "width": 140
-        }
     ]
 
     condition_str = ReportFilter.set_report_filters(filters, 'date_of_visit', True , 'ben_table')
@@ -68,13 +61,12 @@ def execute(filters=None):
     sql_query = f"""
     SELECT
         milestone_category,
-        COUNT(_sc.name) as count,
-        SUM(CASE WHEN (application_submitted = 'No' AND status = 'Open') THEN 1 ELSE 0 END) as open_demands,
-        SUM(CASE WHEN (status = 'Completed' AND application_submitted = 'Yes') THEN 1 ELSE 0 END) as completed_demands,
-        SUM(CASE WHEN (status = 'Completed' AND application_submitted = 'Yes') THEN 1 ELSE 0 END) as closed_demands,
-        SUM(CASE WHEN (status = 'Under process' AND application_submitted = 'Yes') THEN 1 ELSE 0 END) as submitted_demands,
-        SUM(CASE WHEN (status = 'Rejected' AND application_submitted = 'Yes') THEN 1 ELSE 0 END) as rejected_demands,
-        SUM(CASE WHEN (application_submitted = 'No' AND status = 'Open') OR (application_submitted = 'Yes' AND status = 'Under process') THEN 1 ELSE 0 END) as total_demands
+        SUM(CASE WHEN (_sc.status = 'Open') THEN 1 ELSE 0 END) as open_demands,
+        SUM(CASE WHEN (_sc.status = 'Completed') THEN 1 ELSE 0 END) as completed_demands,
+        SUM(CASE WHEN (_sc.status = 'Closed') THEN 1 ELSE 0 END) as closed_demands,
+        SUM(CASE WHEN (_sc.status = 'Under process') THEN 1 ELSE 0 END) as submitted_demands,
+        SUM(CASE WHEN (_sc.status = 'Rejected') THEN 1 ELSE 0 END) as rejected_demands,
+        COUNT(_sc.status) as total_demands
     FROM
         `tabScheme Child` as _sc
     INNER JOIN `tabBeneficiary Profiling` as ben_table on (ben_table.name =  _sc.parent and _sc.parenttype ='Beneficiary Profiling')
