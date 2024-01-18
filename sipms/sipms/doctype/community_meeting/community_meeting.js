bdf1c4d963df37b12f32f90aefbc497c08a6434d
Abhishek kumar <abhishek.suvaidyam@gmail.com>
2024-01-18 02:54:15 +0000

    add

--- sipms/sipms/doctype/community_meeting/community_meeting.js
+++ sipms/sipms/doctype/community_meeting/community_meeting.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2024, suvaidyam and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Community meeting", {
-// 	refresh(frm) {
+frappe.ui.form.on("Community meeting", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+    add_to_beneficary:function(frm){
+        // frappe.new_doc('Beneficiary Profiling', { status: 'Open' })
+        // frappe.set_route('List', 'Beneficiary Profiling');
+        // frappe.set_route(`/beneficiary-profiling/new-beneficiary-profiling/?id=${frm.doc.name_of_the_beneficiary}`)
+        // window.print()
+        id = frm.doc.name
+        // frappe.set_route(['List', 'Beneficiary Profiling', 'Beneficiary Profiling'], { status: 'Open' })
+    }
+});
