1d0a2ffabd7addc91b8ba28fb7ac5aa24fffb9ea
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2024-01-20 06:35:07 +0000

    add changes

--- sipms/api.py
+++ sipms/api.py
@@ -42,9 +42,9 @@ def most_eligible_ben():
             # condition_str = f"{condition_str} "  
         else:
             condition_str = ""
-        get_elegible_ben = f""" SELECT count(name) FROM `tabBeneficiary Profiling` WHERE{condition_str} 1=1"""
+        get_elegible_ben = f""" SELECT count(name) as abc FROM `tabBeneficiary Profiling` WHERE{condition_str} 1=1"""
         all_ben = frappe.db.sql(get_elegible_ben, as_dict=True)
-        sch_ben = {"scheam": scheme , "bencount": all_ben}
+        sch_ben = {"scheam": scheme.name , "bencount": all_ben[0].abc}
         scheam_ben_count.append(sch_ben)
 
     return scheam_ben_count
