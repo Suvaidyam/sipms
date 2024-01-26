92325aaa7a70e8dfc39ac130464fab30d8a7a8d0
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2024-01-26 17:32:19 +0000

    add shedular events

--- sipms/scheduler_events/ben_dob_update.py
+++ sipms/scheduler_events/ben_dob_update.py
@@ -6,7 +6,13 @@ def update_dob_of_ben():
     WHERE DATE_FORMAT(date_of_birth, '%m-%d') = DATE_FORMAT(DATE_ADD(CURDATE(), INTERVAL 1 DAY), '%m-%d');
     """
     data = frappe.db.sql(query, as_dict=True)
-    print(data)
 
     return data
 
+def update_dob_months():
+    query = """UPDATE `tabBeneficiary Profiling`
+    SET completed_age_month = completed_age_month + 1
+    WHERE DATE_FORMAT(date_of_birth, '%m-%d') = DATE_FORMAT(DATE_ADD(CURDATE(), INTERVAL 1 DAY), '%m-%d');
+    """
+    data = frappe.db.sql(query, as_dict=True)
+    return data
