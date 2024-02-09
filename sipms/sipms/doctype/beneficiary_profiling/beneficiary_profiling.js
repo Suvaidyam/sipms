8e6f782a463406e537686567598919dcd1453a2d
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2024-02-09 17:53:45 +0530

    add changes

--- sipms/sipms/doctype/beneficiary_profiling/beneficiary_profiling.js
+++ sipms/sipms/doctype/beneficiary_profiling/beneficiary_profiling.js
@@ -606,6 +606,7 @@ frappe.ui.form.on("Beneficiary Profiling", {
     if(!frappe.user_roles.includes("Admin")){
       if(!frm.doc.__islocal){
         frm.set_df_property('help_desk', 'read_only', 1);
+        frm.set_df_property('date_of_visit', 'read_only', 1);
       }
     }
     //  APPLY Filter in ID DOCUMENT
